12012cd635f45ff4478a5a805441a8bd47f6ed5f
Sérgio Ramada <111106259+ramadageografia@users.noreply.github.com>
2025-11-09 10:24:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-// main minimal loader
-fetch('Assets/Dados/festivals.json').then(r=>r.json()).then(data=>console.log('festivals loaded',data.length)).catch(e=>console.error(e));
+[
+  {
+    "nome": "Hadra Trance Festival",
+    "pais": "França",
+    "continente": "Europa",
+    "vertente": "Full On",
+    "latitude": 44.9,
+    "longitude": 5.4,
+    "flyer": "https://raw.githubusercontent.com/ramadageografia/nomad_maps/main/imagens/imagens/flyer-hadra.jpg",
+    "link": "https://www.hadra.net/"
+  },
+  {
+    "nome": "Ozora Festival",
+    "pais": "Hungria",
+    "continente": "Europa",
+    "vertente": "Progressive",
+    "latitude": 46.63,
+    "longitude": 18.48,
+    "flyer": "https://raw.githubusercontent.com/ramadageografia/nomad_maps/main/imagens/imagens/ozora.jpg",
+    "link": "https://ozorafestival.eu/"
+  }
+]
